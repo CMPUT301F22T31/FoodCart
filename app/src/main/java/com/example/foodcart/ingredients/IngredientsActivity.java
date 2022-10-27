85fdc6c9aecc93987bc558bc774fd7b91f507ece
ahlee2 <ahlee1@ualberta.ca>
2022-10-26 20:49:58 -0600

    Implement: RecipeActivity & Tabbing

--- app/src/main/java/com/example/foodcart/ingredients/IngredientsActivity.java
+++ app/src/main/java/com/example/foodcart/ingredients/IngredientsActivity.java
@@ -1,19 +1,18 @@
 package com.example.foodcart.ingredients;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.foodcart.R;
-import com.example.foodcart.ingredients.AddIngredientFragment;
-import com.example.foodcart.ingredients.CustomIngredientArrayAdapter;
-import com.example.foodcart.ingredients.EditIngredientFragment;
-import com.example.foodcart.ingredients.Ingredient;
+import com.example.foodcart.recipes.RecipeActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -47,6 +46,15 @@ public class IngredientsActivity extends AppCompatActivity implements AddIngredi
         ingredientAdapter = new CustomIngredientArrayAdapter(this, dataList);
         ingredientList.setAdapter(ingredientAdapter);
 
+        final ImageButton RecipeTab = findViewById(R.id.recipes_tab);
+        RecipeTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchActivityIntent = new Intent(getApplicationContext(), RecipeActivity.class);
+                startActivity(switchActivityIntent);
+            }
+        });
+
         // onClick for Add Food Button (floating action + button)
         final FloatingActionButton addFoodButton = findViewById(R.id.add_ingredient_button);
         addFoodButton.setOnClickListener(new View.OnClickListener() {
@@ -67,7 +75,6 @@ public class IngredientsActivity extends AppCompatActivity implements AddIngredi
             }
         });
 
-
     }
 
     // for Add Food Fragment submit
@@ -87,5 +94,6 @@ public class IngredientsActivity extends AppCompatActivity implements AddIngredi
         dataList.set(selected, ingredient);
         ingredientAdapter.notifyDataSetChanged();
     }
+
 }
 
