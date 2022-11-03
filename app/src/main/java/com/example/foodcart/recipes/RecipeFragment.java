89ba8e9e9b13b781127931632b154c3a2bd05565
Arsh <arsh.narkhede@gmail.com>
2022-11-03 16:22:11 -0600

    Scroll View added to all dialog fragments

--- app/src/main/java/com/example/foodcart/recipes/RecipeFragment.java
+++ app/src/main/java/com/example/foodcart/recipes/RecipeFragment.java
@@ -12,6 +12,8 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
