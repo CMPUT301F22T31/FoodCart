01c8810b10b3f9bb93b9bf7e3710215f1e6c6208
AshMarti <andavis@ualberta.ca>
2022-11-04 14:47:09 -0600

    Added JavaDocs to Recipe class

--- app/src/main/java/com/example/foodcart/recipes/Recipe.java
+++ app/src/main/java/com/example/foodcart/recipes/Recipe.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 
+/**
+ * Recipe object with the title, prep time, number of servings,
+ * comments, picture, and category of recipe
+ */
 public class Recipe implements Serializable {
     String title;
     int prep_time;
@@ -17,70 +21,144 @@ public class Recipe implements Serializable {
     ArrayList<Ingredient> ingredientList;
     String picture;
 
+    /**
+     * Gets the title of the recipe
+     * @return the title of the recipe
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the recipe
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Gets the prep time of the recipe
+     * @return the prep time of the recipe
+     */
     public int getPrep_time() {
         return prep_time;
     }
 
+    /**
+     * Sets the prep time of the recipe
+     * @param prep_time
+     */
     public void setPrep_time(int prep_time) {
         this.prep_time = prep_time;
     }
 
+    /**
+     * Gets the number of servings
+     * @return the number of servings for this recipe
+     */
     public int getServings() {
         return servings;
     }
 
+    /**
+     * Sets the number of servings for this recipe
+     * @param servings
+     */
     public void setServings(int servings) {
         this.servings = servings;
     }
 
+    /**
+     * Gets the recipe comments
+     * @return recipe comments
+     */
     public String getComments() {
         return comments;
     }
 
+    /**
+     * Sets the comments for the recipe
+     * @param comments
+     */
     public void setComments(String comments) {
         this.comments = comments;
     }
 
+    /**
+     * Gets the category of the recipe
+     * @return the category of the recipe
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Sets the category of the recipe
+     * @param category
+     */
     public void setCategory(String category){
         this.category = category;
     }
 
+    /**
+     * Gets the list of ingredients for this recipe
+     * @return the list of ingredients for this recipe
+     */
     public ArrayList<Ingredient> getIngredientList() {
         return ingredientList;
     }
 
+    /**
+     * Sets the list of ingredients for this recipe
+     * @param ingredientList
+     */
     public void setIngredientList(ArrayList<Ingredient> ingredientList) {
         this.ingredientList = ingredientList;
     }
 
+    /**
+     * Sets the picture for the recipe
+     * @param picture
+     */
     public void setPicture(String picture) {
         this.picture = picture;
     }
 
+    /**
+     * Gets the picture for the recipe
+     * @return the picture of the recipe
+     */
     public String getPicture() {
         return picture;
     }
 
+    /**
+     * Adds a single ingredient to the
+     * @param ingredient
+     */
     public void addIngredient(Ingredient ingredient) {
         this.ingredientList.add(ingredient);
     }
 
+    /**
+     * Removes an ingredient from the list of ingredients for this recipe
+     * @param ingredient
+     */
     public void removeIngredient(Ingredient ingredient) {
         this.ingredientList.remove(ingredient);
     }
 
+    /**
+     * Constructor for recipe
+     * @param title
+     * @param prep_time
+     * @param servings
+     * @param comments
+     * @param picture
+     * @param category
+     * @param ingredients
+     */
     public Recipe(String title, int prep_time, int servings, String comments,
                   String picture, String category, ArrayList<Ingredient> ingredients)  {
         setTitle(title);
