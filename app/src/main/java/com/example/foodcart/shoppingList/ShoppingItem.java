25a1e7e3b451b305cb38801f234d6b45fdcc2230
AshMarti <andavis@ualberta.ca>
2022-11-20 04:50:28 -0700

    More features in Shopping List

--- app/src/main/java/com/example/foodcart/shoppingList/ShoppingItem.java
+++ app/src/main/java/com/example/foodcart/shoppingList/ShoppingItem.java
@@ -1,5 +1,7 @@
 package com.example.foodcart.shoppingList;
 
+import android.widget.Toast;
+
 import com.example.foodcart.ingredients.Ingredient;
 
 import java.util.Date;
@@ -7,17 +9,8 @@ import java.util.Date;
 public class ShoppingItem extends Ingredient {
 
     /**
-     * The constructor of the Ingredient class. Creates a new
-     * ingredient with the params passed.
-     *
-     * @param description
-     * @param count
-     * @param unit
-     * @param category
-     */
-    /**
-     * The constructor of the Ingredient class. Creates a new
-     * ingredient with the params passed.
+     * The constructor of the ShoppingItem class. Creates a new
+     * shopping list item with the params passed.
      * @param description
      * @param count
      * @param unit
@@ -25,5 +18,35 @@ public class ShoppingItem extends Ingredient {
      */
     public ShoppingItem(String description, int count, String unit, String category) {
         super(description, null, null, count, unit, category);
+    }
+
+    /**
+     * Gets the best before date of an ingredient
+     * @return the best before date of the ingredient
+     */
+    public Ingredient convertToIngredient(Date bestbeforedate, String location) {
+        if (!emptyStringCheck(getDescription(), location, getCount().toString(), getCategory())) {
+            Ingredient ingredient = new Ingredient(getDescription(), bestbeforedate, location, getCount(), getUnit(), getCategory());
+            return ingredient;
+        } else {
+            return null;
+        }
+    }
+
+    private boolean emptyStringCheck(String description, String location, String count, String category) {
+        boolean emptyStringExist = false;
+        if(description.isEmpty()) {
+            emptyStringExist = true;
+        }
+        else if(location.isEmpty()) {
+            emptyStringExist = true;
+        }
+        else if(count.isEmpty()) {
+            emptyStringExist = true;
+        }
+        else if(category.isEmpty()) {
+            emptyStringExist = true;
+        }
+        return emptyStringExist;
     }
 }
